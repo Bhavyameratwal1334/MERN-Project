f72071e0af8a918e732f800807f34c5356a60f60
Bhavyameratwal1334 <bhavya24meratwal@gmail.com>
2024-07-09 18:42:51 +0530

    Creted the sig_up page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import { response } from "express";
 import bcryptjs from "bcryptjs";
-import User from "../models/user.model.js";7
+import User from "../models/user.model.js";
 
 export const signup = async (req , res, next) => {
-    const { username,email,password} = req.body;
+    const { username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
     const newUser = new User({username,email,password: hashedPassword});
+    
     try{
         await newUser.save();
         res.status(201).json("User Created Succesfully");
