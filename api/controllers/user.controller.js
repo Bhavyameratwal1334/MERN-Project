b368824a74881f3b856b0c3dca14110830811022
Bhavyameratwal1334 <bhavya24meratwal@gmail.com>
2024-07-15 23:01:26 +0530

    Completed Profile page

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import errorHandler from "../utils/error.js";
 import bcryptjs from 'bcryptjs';
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
+
 
 export const test = (req ,res ) => {
     res.json({
@@ -43,5 +45,20 @@ export const deleteUser = async (req, res, next) =>{
         res.status(200).json("User has Been Deleted!.");
     } catch (error ){
         next(error);
+    }
+}
+
+
+export const getUserListing = async (req, res, next) => {
+    if(req.user.id !== req.params.id ) {
+        return next(errorHandler(401,'You can only view your listings'));
+    } else {
+        try {
+            const listings = await Listing.find({userRef : req.params.id });
+            res.status(200).json(listings);
+
+        } catch (error) {
+            next(error)
+        }
     }
 }
