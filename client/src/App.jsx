44d5daa49fc77340176a736345e723b52f887f63
Bhavyameratwal1334 <bhavya24meratwal@gmail.com>
2024-07-16 17:01:40 +0530

    Completed the Listing Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@
  import PrivateRoute from './components/PrivateRoute'
  import CreateListing from './pages/CreateListing'
  import UpdateListing from './pages/UpdateListing'
+ import Listing from './pages/Listing'
 
  export default function App() {
    return (
@@ -20,6 +21,7 @@
         <Route path="/sign-in" element={ <Signin/> } />
         <Route path="/sign-up" element={ <Signup/> } />
         <Route path="/about" element={ <About/> } />
+        <Route path="/listing/:listingId" element={ <Listing/> } />
         <Route element={ <PrivateRoute /> }>
           <Route path="/profile" element={ <Profile/> } />
           <Route path="/create-listing" element={ <CreateListing/> } />
